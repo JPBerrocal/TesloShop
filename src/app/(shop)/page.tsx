952a9bdf3c74873807918ae467d0f7b3c56b30bf
JPBR <jpberrocal@gmail.com>
2024-07-18 22:07:26 -0600

    Creacion de rutas de navegacion

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,9 +2,9 @@ import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World!!!</h1>
+    <div>
+      <div>Hello World!!!</div>
       <h1 className={titleFont.className}>Hello World but different!!!</h1>
-    </main>
+    </div>
   );
 }
