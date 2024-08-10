e66f0b303f98f6739448b2c29cec1f02eff468f2
JPBR <jpberrocal@gmail.com>
2024-08-09 23:53:31 -0600

    Fin seccion 15

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,5 +3,9 @@ export default function ShopLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <main className="bg-sky-400 min-h-screen">{children}</main>;
+  return (
+    <main className="flex justify-center">
+      <div className="w-full sm:w-[350px] px-10">{children}</div>
+    </main>
+  );
 }
