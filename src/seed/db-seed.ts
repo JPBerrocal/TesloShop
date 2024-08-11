034ffeb38c5271f32881204aaac5f10df8db9c1a
JPBR <jpberrocal@gmail.com>
2024-08-10 22:59:01 -0600

    Finished section 16. Database Generation

---
+++ src/seed/db-seed.ts
@@ -0,0 +1,60 @@
+import { initialData } from "./seed";
+import prisma from "../lib/prisma";
+
+async function main() {
+  await Promise.all([
+    await prisma.productImage.deleteMany(),
+    await prisma.product.deleteMany(),
+    await prisma.category.deleteMany(),
+  ]);
+
+  const { categories, products } = initialData;
+
+  //categories
+  const categoriesData = categories.map((name) => ({
+    name,
+  }));
+
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
+
+  //relacion category-product
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoryMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+
+  //products
+  products.forEach(async (product) => {
+    const { images, type, ...cleanProduct } = product;
+    const productDb = await prisma.product.create({
+      data: {
+        ...cleanProduct,
+        categoryId: categoryMap[type],
+      },
+    });
+
+    //images
+    const imagesData = images.map((image) => ({
+      productId: productDb.id,
+      url: image,
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    });
+  });
+
+  console.log("Seed executed");
+}
+
+(() => {
+  if (process.env.NODE_ENV === "production") {
+    console.log("Skipping seed");
+    return;
+  }
+  main();
+})();
